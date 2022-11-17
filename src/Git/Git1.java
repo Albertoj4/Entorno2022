4c4834964f392c62e00fef08810600d87d5c0632
alumno <alumno@DESKTOP-UE84JL7>
2022-11-17 18:34:17 +0100

    descripcion

--- src/Git/Git1.java
+++ src/Git/Git1.java
@@ -3,8 +3,9 @@ package Git;
 public class Git1 {
 
 	public static void main(String[] args) {
-		// TODO Esbozo de método generado automáticamente
-
+		System.out.println("Hola");
+		System.out.println("Adios");
+		
 	}
 
 }
